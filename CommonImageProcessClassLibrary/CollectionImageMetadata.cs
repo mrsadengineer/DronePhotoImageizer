c93002736bfedcae05695da4c4ed93c748763a33
Sam <dsaeresearch@gmail.com>
2020-12-23 23:47:07 -0500

    Added Console App; Added Classlibrary for Working with Image Metadata.

---
+++ CommonImageProcessClassLibrary/CollectionImageMetadata.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonImageProcessClassLibrary
+{
+    class CollectionImageMetadata
+    {
+
+        //totalnumber
+
+        //gps_related
+        //max in lat
+        //min in lat
+
+        //max in long
+        //min in long
+
+        //matrix of GPS
+    }
+}
---
+++ DronePhotImageizer.ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DronePhotImageizer.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exploring EXIF Image Metadata");
+
+
+            DisplayImageGPSMetadata display = new DisplayImageGPSMetadata();
+        }
+    }
+}
